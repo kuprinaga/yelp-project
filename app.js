bcb0bc228a38471a3bf60f6106abad33ce505fda
Anastasia Kuprina <nastya.kuprina@gmail.com>
2016-10-16 12:34:47 +0300

    Add basic MongoDB connectivity

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var campgrounds = [
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
-        {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
-        {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
-        {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
-        {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
-        {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"}
-    ];
+// var campgrounds = [
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
+//         {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
+//         {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"},
+//         {name: "Park", image: "http://www.sanctuaryretreats.com/media/2488490/kenya_private_camping.jpg"},
+//         {name: "Mountain", image: "http://www.dailyaddict.com.au/wp-content/uploads/2013/02/paperbark.jpg"},
+//         {name: "Desert", image: "http://jewishexponent.com/sites/default/files/styles/issue_main/public/camp%20cabins.jpg?itok=kfbkGHx3"}
+//     ];
+//TO DO: Change in all places where campgrounds was used
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yelp");
+
+var yelpSchema = new mongoose.Schema({
+   name: String,
+   place: String
+});
+
+var Campground = mongoose.model("Campground", yelpSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -44,7 +54,7 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var newCampground = {name: name, image: image};
     campgrounds.push(newCampground);
-//redirect back to campgrounds
+   //redirect back to campgrounds
     res.redirect("/campgrounds");
 });
 
